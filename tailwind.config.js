3eb1b16d0604644b41fdc0b22c4832ad740e5abf
hsjung2015 <hsjung201558@gmail.com>
2022-11-16 16:26:41 +0900

    navbar 및 css 수정작업

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#F96162',
+      },
+    },
+  },
+  plugins: [],
+}
