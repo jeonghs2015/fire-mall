bdbb0d254751e9d83f2feaeaa6b3e52e0a5e69d8
hsjung2015 <hsjung201558@gmail.com>
2022-11-17 17:21:33 +0900

    isAdmin 어드민 구분 기능 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,6 +6,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase, ref, child, get, update } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
 
 export function login() {
   return signInWithPopup(auth, provider)
@@ -28,7 +30,19 @@ export function logout() {
 }
 
 export function onUserStateChange(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+async function adminUser(user) {
+    return get(ref(database, 'admins')).then((snapshot) => {
+        if(snapshot.exists()){
+            const admins = snapshot.val();
+            const isAdmin = admins.includes(user.uid);
+            return {...user, isAdmin};
+        }
+        return user;
+    });
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,10 @@ export default function Navbar() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    onUserStateChange(setUser);
+    onUserStateChange(user => {
+      console.log(user);
+      setUser(user);
+    });
   }, []);
  
   return (
