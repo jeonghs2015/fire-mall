5af6e76525fd80194f4e952352f1a857606b9859
hsjung2015 <hsjung201558@gmail.com>
2022-11-22 14:34:07 +0900

    상품 리스트 ui 수정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ProductCard({product: {id, image, title, category, price},
 }) {
-  return <li>
-    <img src={image} alt={title} />
-    <div>
+  return <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+    <img className='w-full' src={image} alt={title} />
+    <div className='mt-2 px-2 text-lg flex justify-between items-center'>
         <h3>{title}</h3>
         <p>{`₩${price}`}</p>
     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -9,7 +9,7 @@ export default function Products() {
     <>
         {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-        <ul>
+        <ul className='grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4'>
             {products && products.map((product) =>( 
                 <ProductCard key={product.id} product={product} />
              ))}
