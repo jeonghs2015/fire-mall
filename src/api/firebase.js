ed65e961dfbef5b5a4e7b2d1e25c7d69a23a287b
hsjung2015 <hsjung201558@gmail.com>
2022-11-23 14:26:45 +0900

    cart 관련 함수 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -7,7 +7,7 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getDatabase, ref, set, get } from 'firebase/database';
+import { getDatabase, ref, set, get, remove } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -66,4 +66,21 @@ export async function getProducts() {
     }
     return [];
   })
+}
+
+export async function getCart(userId) {
+    return get(ref(database, `carts/${userId}`))
+    .then(snapshot => {
+      const items = snapshot.val() || {};
+      return Object.values(items);
+    })
+}
+
+
+export async function addOrUpdateToCart(userId, product) {
+    return set(ref(database, `carts/${userId}/${product.id}`));
+}
+
+export async function removeFromCart(userId, productId) {
+  return remove(ref(database, `carts/${userId}/${productId}`));
 }
