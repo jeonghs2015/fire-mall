7d5097bcf5157867ad05ea614e738640ffa749ac
hsjung2015 <hsjung201558@gmail.com>
2022-11-30 17:27:23 +0900

    useCart hook 추가

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,21 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { addOrUpdateToCart, getCart, removeFromCart } from "../api/firebase";
+import { useAuthContext } from '../context/AuthContext';
+
+export default function useCart() {
+    const { uid } = useAuthContext();
+    const queryClient = useQueryClient();
+    const cartQuery = useQuery(['carts', uid || ''], () => getCart(uid), {
+        enabled: !!uid,
+    });
+
+    const addOrUpdateItem - useMutation(
+        (product) => addOrUpdateToCart(uid, product),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(['carts', uid]);
+            },
+            
+        }
+    )
+}
